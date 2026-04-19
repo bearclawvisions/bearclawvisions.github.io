3b1b90feeb8fd45471d7cb173fb4af8b9abbef63
bearclawvisions <113764031+bearclawvisions@users.noreply.github.com>
2026-04-19 14:45:47 +0200

    adjust admin terminal component

--- src/components/TerminalProjects.vue
+++ src/components/TerminalProjects.vue
@@ -5,4 +5,21 @@
 <template>
   <h2 class="animate-bounce">LOADING...</h2>
   <p>Sector 7 projects are currently encrypted.</p>
+
+  <!--  <section>-->
+  <!--    <h2>> EXPERIENCE_LOG</h2>-->
+  <!--    <div class="experience-container">-->
+  <!--      <div class="experience-item">-->
+  <!--        <div class="pink-li"></div>-->
+  <!--        <h3>Lead Architect @ GridRunner Inc.</h3>-->
+  <!--        <p>2024 - PRESENT</p>-->
+  <!--        <p>Developing high-performance UI systems for neural link interfaces.</p>-->
+  <!--      </div>-->
+  <!--      <div class="experience-item">-->
+  <!--        <div class="pink-li"></div>-->
+  <!--        <h3>Junior Dev @ ByteCorp</h3>-->
+  <!--        <p>2022 - 2024</p>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--  </section>-->
 </template>
--- src/components/TerminalUser.vue
+++ src/components/TerminalUser.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import {terminal, admin} from "@/assets/data.ts";
+import {terminal, admin, skills} from "@/assets/data.ts";
 
 const adminGeneric = Object.entries(admin).slice(0, 4);
-// const adminSupplemental = Object.entries(admin).slice(3, 4);
-const hobbies = admin.hobbies;
+const certificates = admin.certificates;
+const frontendSkills = skills.frontend;
+const backendSkills = skills.backend;
+const tools = skills.tools;
+const other = skills.softSkills;
 </script>
 
 <template>
-  <section>
-    <h2 class="blinking-cursor">{{terminal}} INITIALIZING_IDENTITY</h2>
+    <h2 class="blinking-cursor">
+      {{terminal}} INITIALIZING_IDENTITY
+    </h2>
+    
     <div class="identity-container">
       
       <div class="mini-container">
@@ -23,51 +28,49 @@ const hobbies = admin.hobbies;
       </div>
 
       <div class="mini-container">
-        <h3>// HOBBIES</h3>
+        <h3>// CERTIFICATIONS</h3>
         <div class="tag-container">
-          <span v-for="hobby in hobbies" :key="hobby" class="tag-item">
-            {{ hobby }}
+          <span v-for="certificate in certificates" :key="certificate" class="tag-item">
+            {{ certificate }}
           </span>
         </div>
       </div>
 
-<!--      <div class="mini-container">-->
-<!--        <h3>// TECHNICAL_STACK</h3>-->
-<!--        <div class="tag-container">-->
-<!--            <span v-for="skill in skills" :key="skill"-->
-<!--                  class="tag-item">-->
-<!--              {{ skill }}-->
-<!--            </span>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--      -->
-<!--      <div class="mini-container">-->
-<!--        <h3>// TECHNICAL_STACK</h3>-->
-<!--        <div class="tag-container">-->
-<!--            <span v-for="skill in skills" :key="skill"-->
-<!--                  class="tag-item">-->
-<!--              {{ skill }}-->
-<!--            </span>-->
-<!--        </div>-->
-<!--      </div>-->
+      <div class="mini-container">
+        <h3>// FRONTEND_STACK</h3>
+        <div class="tag-container">
+          <span v-for="frontSkill in frontendSkills" :key="frontSkill" class="tag-item">
+            {{ frontSkill }}
+          </span>
+        </div>
+      </div>
+
+      <div class="mini-container">
+        <h3>// BACKEND_STACK</h3>
+        <div class="tag-container">
+          <span v-for="backendSkill in backendSkills" :key="backendSkill" class="tag-item">
+            {{ backendSkill }}
+          </span>
+        </div>
+      </div>
+
+      <div class="mini-container">
+        <h3>// TOOL_STACK</h3>
+        <div class="tag-container">
+          <span v-for="tool in tools" :key="tool" class="tag-item">
+            {{ tool }}
+          </span>
+        </div>
+      </div>
+
+      <div class="mini-container">
+        <h3>// OTHER_STACK</h3>
+        <div class="tag-container">
+          <span v-for="item in other" :key="item" class="tag-item">
+            {{ item }}
+          </span>
+        </div>
+      </div>
       
     </div>
-  </section>
-
-  <!--  <section>-->
-  <!--    <h2>> EXPERIENCE_LOG</h2>-->
-  <!--    <div class="experience-container">-->
-  <!--      <div class="experience-item">-->
-  <!--        <div class="pink-li"></div>-->
-  <!--        <h3>Lead Architect @ GridRunner Inc.</h3>-->
-  <!--        <p>2024 - PRESENT</p>-->
-  <!--        <p>Developing high-performance UI systems for neural link interfaces.</p>-->
-  <!--      </div>-->
-  <!--      <div class="experience-item">-->
-  <!--        <div class="pink-li"></div>-->
-  <!--        <h3>Junior Dev @ ByteCorp</h3>-->
-  <!--        <p>2022 - 2024</p>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </section>-->
 </template>
